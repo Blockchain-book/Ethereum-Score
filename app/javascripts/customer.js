c669f0dd98426b4c2f89717b15c1bde801cc0605
陈宇峰 <yufengcode@gmail.com>
2016-12-25 00:02:20 +0800

    实现用户购买商品，查看已经购买的商品。

--- app/javascripts/customer.js
+++ app/javascripts/customer.js
@@ -27,3 +27,36 @@ function transferScoreToAnotherFromCustomer() {
     });
 }
 
+//客户购买商品
+function buyGood() {
+    var goodId = document.getElementById("goodId").value;
+    contractAddr.buyGood(currentAccount, goodId, {from: account});
+    var eventBuyGood = contractAddr.BuyGood();
+    eventBuyGood.watch(function (error, event) {
+        console.log(event.args.message);
+        //alert(event.args.message);
+
+        if(event.args.isSuccess){
+            contractAddr.buyGoodSuccess(currentAccount, goodId, {from: account});
+        }
+        else{
+            alert(event.args.message);
+        }
+
+        eventBuyGood.stopWatching();
+    });
+}
+
+//查看已经购买的商品
+function getGoodsByCustomer() {
+    contractAddr.getGoodsByCustomer.call(currentAccount, {from: account}).then(function (result) {
+        console.log(result.length);
+        console.log(result);
+
+        for (var i = 0; i < result.length; i++){
+            var temp = hexCharCodeToStr(result[i]);
+            console.log(temp);
+        }
+    });
+}
+
